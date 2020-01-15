7931bc1ba4ec86e7280837ba485dadd60b132a23
shoonlaiyee <40440094@live.napier.ac.uk>
2020-01-15 10:33:34 +0630

    Updated Main

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -6,11 +6,29 @@ public class App
 {
     public static void main(String[] args)
     {
+        // Create new Application
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+
+        // Disconnect from database
+        a.disconnect();
+    }
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
+
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect()
+    {
         try
         {
             // Load Database driver
             Class.forName("com.mysql.jdbc.Driver");
-            System.out.println("found it");
         }
         catch (ClassNotFoundException e)
         {
@@ -18,11 +36,8 @@ public class App
             System.exit(-1);
         }
 
-        // Connection to the database
-        Connection con = null;
-        int retries = 100;
-        int i=0;
-        for (i = 0; i < retries; ++i)
+        int retries = 10;
+        for (int i = 0; i < retries; ++i)
         {
             System.out.println("Connecting to database...");
             try
@@ -32,14 +47,11 @@ public class App
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
-                // Wait a bit
-                Thread.sleep(10000);
-                // Exit for loop
                 break;
             }
             catch (SQLException sqle)
             {
-                System.out.println("Fail to connect to database attempt " + Integer.toString(i));
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
                 System.out.println(sqle.getMessage());
             }
             catch (InterruptedException ie)
@@ -47,7 +59,13 @@ public class App
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
+    }
 
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
         if (con != null)
         {
             try
@@ -61,4 +79,5 @@ public class App
             }
         }
     }
+
 }
